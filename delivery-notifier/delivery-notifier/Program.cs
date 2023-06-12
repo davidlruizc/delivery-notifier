5339eb19fde44865b18757dc24c4e411b0d85fcd
David Ruiz <davidl-ruizc@outlook.com>
2023-06-12 14:45:31 -0500

    add gitignore and initialize project

---
+++ delivery-notifier/delivery-notifier/Program.cs
@@ -0,0 +1,15 @@
+namespace delivery_notifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}

66a6668b738a471c343c425766278d5942fb9a79
David Ruiz <davidl-ruizc@outlook.com>
2023-06-12 15:35:41 -0500

    add restaurant model

---
+++ delivery-notifier/Utilities/Domain/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Utilities.Domain
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+
+        public void InitializeBase()
+        {
+            Id = Guid.NewGuid();
+            Created = DateTime.Now.ToUniversalTime();
+        }
+    }
+}

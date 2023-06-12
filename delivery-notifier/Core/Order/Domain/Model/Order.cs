a1eb64a7a45fcf7a023baace2171d96f1789d836
David Ruiz <davidl-ruizc@outlook.com>
2023-06-12 15:52:11 -0500

    add order model

---
+++ delivery-notifier/Core/Order/Domain/Model/Order.cs
@@ -0,0 +1,27 @@
+﻿using Utilities.Domain;
+
+namespace Core.Order.Domain.Model
+{
+    public class Order : BaseEntity
+    {
+        public Guid ProductId { get; set; }
+        public int SystemProvider { get; set; }
+
+        private Order(Guid id, Guid productId, int systemProvider)
+        {
+            Id = id;
+            ProductId = productId;
+            SystemProvider = systemProvider;
+        }
+
+        internal void Initialize()
+        {
+            InitializeBase();
+        }
+
+        public static Order Of(Guid id, Guid productId, int systemProvider)
+        {
+            return new Order(id, productId, systemProvider);
+        }
+    }
+}

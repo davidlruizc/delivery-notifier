d7b0a85f348caa89d6a76d2e6a129261092864b6
David Ruiz <davidl-ruizc@outlook.com>
2023-06-14 15:55:53 -0500

    add product configuration

---
+++ delivery-notifier/Infrastructure/EntityFramework/Configurations/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Core.Product.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Created).IsRequired();
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.Image).IsRequired();
+            builder.Property(p => p.Price).IsRequired();
+        }
+    }
+}

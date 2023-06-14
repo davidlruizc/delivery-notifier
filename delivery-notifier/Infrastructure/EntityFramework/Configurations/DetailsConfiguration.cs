4cf9c8d98c5e3001e69fedaf5db8a00a3b8b0354
David Ruiz <davidl-ruizc@outlook.com>
2023-06-14 11:33:22 -0500

    add details and orders configuration

---
+++ delivery-notifier/Infrastructure/EntityFramework/Configurations/DetailsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Core.Order.Domain.Model;
+using Core.Product.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configurations
+{
+    internal class DetailsConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            builder.ToTable("Details");
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.OrderId).IsRequired();
+            builder.Property(p => p.ProductId).IsRequired();
+            builder.Property(p => p.Created).IsRequired();
+            builder.HasOne<Product>().WithMany().HasForeignKey(x => x.ProductId);
+        }
+    }
+}
---
+++ delivery-notifier/Infrastructure/EntityFramework/Configurations/OrdersConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Core.Order.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configurations
+{
+    internal class OrdersConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Status).IsRequired();
+            builder.Property(p => p.Created).IsRequired();
+            builder.Property(p => p.SystemProvider).IsRequired();
+            builder.Property(p => p.RestaurantId).IsRequired();
+            builder.Property(p => p.UserId).IsRequired();
+            builder.Property(p => p.EstimatedTime).IsRequired();
+            builder.Property(p => p.Total).IsRequired();
+        }   
+    }
+}

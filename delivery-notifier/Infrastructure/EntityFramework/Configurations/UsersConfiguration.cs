07843bcad431e5499642e9c8bb6317b60fde8905
David Ruiz <davidl-ruizc@outlook.com>
2023-06-14 19:09:19 -0500

    add users configuration

---
+++ delivery-notifier/Infrastructure/EntityFramework/Configurations/UsersConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Core.Users.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityFramework.Configurations
+{
+    internal class UsersConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Name)
+                .IsRequired();
+            builder.Property(p => p.Email)
+                .IsRequired();
+            builder.Property(p => p.Created).IsRequired();
+        }
+    }
+}

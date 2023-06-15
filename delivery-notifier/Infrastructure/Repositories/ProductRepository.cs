e8a3e22177148096402aebc4b26673e585e82aa1
David Ruiz <davidl-ruizc@outlook.com>
2023-06-14 19:44:44 -0500

    add product repository

---
+++ delivery-notifier/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Product.Domain.Model;
+using Core.Product.Domain.Services;
+using Infrastructure.EntityFramework;
+using Utilities.Repository;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : EntityFrameworkRepository<DeliveryNotifierContext>, IProductRepository
+    {
+        public ProductRepository(DeliveryNotifierContext context) : base(context)
+        {
+        }
+
+        public IList<Product> GetProductsInList(IList<Guid> ids)
+        {
+            return dbContext.Products.Where(x => ids.Contains(x.Id)).ToList();
+        }
+    }
+}

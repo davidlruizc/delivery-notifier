347e193a159595634782cf82b1fa022fcbde5df2
David Ruiz <davidl-ruizc@outlook.com>
2023-06-14 21:59:44 -0500

    add orders extentions

---
+++ delivery-notifier/delivery-notifier/Extensions/EntityFrameworkExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace delivery_notifier.Extensions
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static void AddEntityFrameworkServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DeliveryNotifierContext>(options =>
+                           options.UseNpgsql(configuration.GetConnectionString("DbConnection")));
+        }
+    }
+}
---
+++ delivery-notifier/delivery-notifier/Extensions/OrderExtension.cs
@@ -0,0 +1,15 @@
+﻿using Core.Order.App;
+using Core.Order.Domain.Services;
+using Infrastructure.Repositories;
+
+namespace delivery_notifier.Extensions
+{
+    public static class OrderExtension
+    {
+        public static void AddOrderServices(this IServiceCollection services)
+        {
+            services.AddScoped<OrderService>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+    }
+}

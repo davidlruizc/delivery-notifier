2d341f72e05bb73cb81afc05a029dff1ce8cd21b
David Ruiz <davidl-ruizc@outlook.com>
2023-06-14 22:24:07 -0500

    add order controller

---
+++ delivery-notifier/delivery-notifier/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Core.Order.App;
+using Core.Order.App.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace delivery_notifier.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderService orderService;
+
+        public OrderController(OrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        [HttpPost("create-order")]
+        public OrderDTO CreateOrder(CreateOrderDTO dto)
+        {
+            return orderService.CreateOrder(dto);
+        }
+
+        [HttpPost("edit-status-order")]
+        public OrderDTO EditStatusOrder(EditStatusOrderDTO dto)
+        {
+            return orderService.EditStatusOrder(dto);
+        }
+    }
+}
